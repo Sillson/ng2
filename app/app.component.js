da790d225fb4b55d199f8f2ad199b44f9972f6ef
Stuart Illson <sillson88@gmail.com>
2016-02-03 20:48:54 -0800

    simple interface

--- app/app.component.js
+++ app/app.component.js
@@ -9,7 +9,7 @@ System.register(['angular2/core'], function(exports_1) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
     var core_1;
-    var AppComponent;
+    var AppComponent, BEERS;
     return {
         setters:[
             function (core_1_1) {
@@ -18,22 +18,33 @@ System.register(['angular2/core'], function(exports_1) {
         execute: function() {
             AppComponent = (function () {
                 function AppComponent() {
-                    this.title = 'Kegalytics v2';
-                    this.beer = {
-                        id: 1,
-                        name: 'Red Chair NWPA'
-                    };
+                    this.title = 'G5 Kegalytics v2';
+                    this.beers = BEERS;
                 }
+                AppComponent.prototype.onSelect = function (beer) { this.selectedBeer = beer; };
                 AppComponent = __decorate([
                     core_1.Component({
                         selector: 'my-app',
-                        template: "\n              <h1>{{title}}</h1>\n              <h2>{{beer.name}} details!</h2>\n              <div><label>id: </label>{{beer.id}}</div>\n              <div>\n                <label>name: </label>\n                <div><input [(ngModel)]=\"beer.name\" placeholder=\"name\"></div>\n              </div>\n            "
+                        template: "\n    <h1>{{title}}</h1>\n    <h2>Deschutes Beers</h2>\n    <ul class=\"beers\">\n      <li *ngFor=\"#beer of beers\"\n        [class.selected]=\"beer === selectedBeer\"\n        [class.high-score]=\"beer.rating > 75\"\n        (click)=\"onSelect(beer)\">\n        <span class=\"badge\">{{beer.rating}}</span> {{beer.name}}\n      </li>\n    </ul>\n    <div *ngIf=\"selectedBeer\">\n      <h2>{{selectedBeer.name}} details!</h2>\n      <div><label>id: </label>{{selectedBeer.id}}</div>\n      <div>\n        <label>name: </label>\n        <input [(ngModel)]=\"selectedBeer.name\" placeholder=\"name\"/>\n      </div>\n      <div>\n        <label>rating: </label>\n        <input [(ngModel)]=\"selectedBeer.rating\" placeholder=\"rating\"/>\n      </div>\n    </div>\n  ",
+                        styles: ["\n    .selected {\n      background-color: #CFD8DC !important;\n      color: white;\n    }\n    .beers {\n      margin: 0 0 2em 0;\n      list-style-type: none;\n      padding: 0;\n      width: 15em;\n    }\n    .beers li {\n      cursor: pointer;\n      position: relative;\n      left: 0;\n      background-color: #EEE;\n      margin: .5em;\n      padding: .3em 0em;\n      height: 1.6em;\n      border-radius: 4px;\n    }\n    .beers li.selected:hover {\n      color: white;\n    }\n    .beers li:hover {\n      color: #607D8B;\n      background-color: #EEE;\n      left: .1em;\n    }\n    .beers .text {\n      position: relative;\n      top: -3px;\n    }\n    .beers .badge {\n      display: inline-block;\n      font-size: small;\n      color: white;\n      padding: 0.8em 0.7em 0em 0.7em;\n      background-color: #607D8B;\n      line-height: 1em;\n      position: relative;\n      left: -1px;\n      top: -4px;\n      height: 1.8em;\n      margin-right: .8em;\n      border-radius: 4px 0px 0px 4px;\n    }\n    .beers .high-score .badge {\n      background-color: green;\n    }\n  "]
                     }), 
                     __metadata('design:paramtypes', [])
                 ], AppComponent);
                 return AppComponent;
             })();
             exports_1("AppComponent", AppComponent);
+            BEERS = [
+                { "id": 11, "name": "Red Chair NWPA", "rating": 91 },
+                { "id": 12, "name": "Abyss", "rating": 86 },
+                { "id": 13, "name": "Inversion", "rating": 51 },
+                { "id": 14, "name": "Bachelor Bitter", "rating": 81 },
+                { "id": 15, "name": "Chain Breaker", "rating": 25 },
+                { "id": 16, "name": "Mirror Pond", "rating": 74 },
+                { "id": 17, "name": "Black Butter Porter", "rating": 68 },
+                { "id": 18, "name": "Fresh Squeezed", "rating": 95 },
+                { "id": 19, "name": "Chasin Freshies", "rating": 93 },
+                { "id": 20, "name": "Hop Henge", "rating": 87 }
+            ];
         }
     }
 });
--- app/app.component.ts
+++ app/app.component.ts
@@ -1,28 +1,102 @@
 import {Component} from 'angular2/core';
-
-@Component({
-    selector: 'my-app',
-    template:`
-              <h1>{{title}}</h1>
-              <h2>{{beer.name}} details!</h2>
-              <div><label>id: </label>{{beer.id}}</div>
-              <div>
-                <label>name: </label>
-                <div><input [(ngModel)]="beer.name" placeholder="name"></div>
-              </div>
-            `
-})
-
-export class AppComponent {
-  public title = 'Kegalytics v2';
-  public beer: Beer = {
-  id: 1,
-  name: 'Red Chair NWPA'
-};
-}
-
 interface Beer {
   id: number;
   name: string;
   rating: number;
 }
+@Component({
+  selector: 'my-app',
+  template:`
+    <h1>{{title}}</h1>
+    <h2>Deschutes Beers</h2>
+    <ul class="beers">
+      <li *ngFor="#beer of beers"
+        [class.selected]="beer === selectedBeer"
+        [class.high-score]="beer.rating > 75"
+        (click)="onSelect(beer)">
+        <span class="badge">{{beer.rating}}</span> {{beer.name}}
+      </li>
+    </ul>
+    <div *ngIf="selectedBeer">
+      <h2>{{selectedBeer.name}} details!</h2>
+      <div><label>id: </label>{{selectedBeer.id}}</div>
+      <div>
+        <label>name: </label>
+        <input [(ngModel)]="selectedBeer.name" placeholder="name"/>
+      </div>
+      <div>
+        <label>rating: </label>
+        <input [(ngModel)]="selectedBeer.rating" placeholder="rating"/>
+      </div>
+    </div>
+  `,
+  styles:[`
+    .selected {
+      background-color: #CFD8DC !important;
+      color: white;
+    }
+    .beers {
+      margin: 0 0 2em 0;
+      list-style-type: none;
+      padding: 0;
+      width: 15em;
+    }
+    .beers li {
+      cursor: pointer;
+      position: relative;
+      left: 0;
+      background-color: #EEE;
+      margin: .5em;
+      padding: .3em 0em;
+      height: 1.6em;
+      border-radius: 4px;
+    }
+    .beers li.selected:hover {
+      color: white;
+    }
+    .beers li:hover {
+      color: #607D8B;
+      background-color: #EEE;
+      left: .1em;
+    }
+    .beers .text {
+      position: relative;
+      top: -3px;
+    }
+    .beers .badge {
+      display: inline-block;
+      font-size: small;
+      color: white;
+      padding: 0.8em 0.7em 0em 0.7em;
+      background-color: #607D8B;
+      line-height: 1em;
+      position: relative;
+      left: -1px;
+      top: -4px;
+      height: 1.8em;
+      margin-right: .8em;
+      border-radius: 4px 0px 0px 4px;
+    }
+    .beers .high-score .badge {
+      background-color: green;
+    }
+  `]
+})
+export class AppComponent {
+  public title = 'G5 Kegalytics v2';
+  public beers = BEERS;
+  public selectedBeer: Beer;
+  onSelect(beer: Beer) { this.selectedBeer = beer; }
+}
+var BEERS: BEER[] = [
+  { "id": 11, "name": "Red Chair NWPA", "rating": 91 },
+  { "id": 12, "name": "Abyss", "rating": 86 },
+  { "id": 13, "name": "Inversion", "rating": 51 },
+  { "id": 14, "name": "Bachelor Bitter", "rating": 81 },
+  { "id": 15, "name": "Chain Breaker", "rating": 25 },
+  { "id": 16, "name": "Mirror Pond", "rating": 74 },
+  { "id": 17, "name": "Black Butter Porter", "rating": 68 },
+  { "id": 18, "name": "Fresh Squeezed", "rating": 95 },
+  { "id": 19, "name": "Chasin Freshies", "rating": 93 },
+  { "id": 20, "name": "Hop Henge", "rating": 87 }
+];
